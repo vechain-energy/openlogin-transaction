6c472ce528f1f5288eece399d072bcb32b038ebf
inspireme247 <breakthesoundbarriers@gmail.com>
2023-02-09 11:21:04 +0000

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 // get your client id from https://dashboard.web3auth.io/home/web3auth
-export const WEB3AUTH_CLIENT_ID = 'BNzXNuZuTXY7mUlPRqDcE3RFUFgKSQXGQEu2TvJoatWNr4c_BburI3pH_x3n_9cvD7TVIwn4qsZv5CFIKAgk4r0'
+export const WEB3AUTH_CLIENT_ID = 'BCuphgqhJCIQacskzbgn5S3ic1S3kuqIUOKnlDqPaDP2Mlxmrne_qT7TC43VK'
 export const WEB3AUTH_NEWORK = 'testnet'
